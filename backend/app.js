ba8d6bba3039895ea5937b68a83157e2eba52182
GilsonFerreira <107324541+GilsonFerreira@users.noreply.github.com>
2025-07-12 20:15:50 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
-const routes = require('./routes');
 const cors = require('cors');
+const routes = require('./routes');
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
