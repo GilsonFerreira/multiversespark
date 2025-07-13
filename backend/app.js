6e25d8aac847141a727028fe7480ad5f55cad967
GilsonFerreira <107324541+GilsonFerreira@users.noreply.github.com>
2025-07-12 21:20:52 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,15 @@ app.use(express.json());
 app.use('/api', routes);
 
 const port = process.env.PORT || 3000;
+const path = require('path');
+
+// Servir os arquivos estáticos da build do React
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Redirecionar qualquer rota não-API para o index.html do React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 app.listen(port, () => {
   console.log(`🚀 Backend rodando na porta ${port}`);
 });
